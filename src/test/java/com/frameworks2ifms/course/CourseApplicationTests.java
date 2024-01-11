5f6bf5ea838c9507600b46cff9e5a591f37a3b72
heyderprof <heyder.ramos@ifms.edu.br>
2024-01-11 15:23:54 -0300

    project created

---
+++ src/test/java/com/frameworks2ifms/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frameworks2ifms.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
